9aa8241ddc177ba48a0f6a16fae42ca095b06aa4
Diego M. <morfidal@gmail.com>
2023-10-17 00:04:43 +0200

    Login route added, AuthView created and basic styles for login

--- src/auth/AuthView.vue
+++ src/auth/AuthView.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    <form action="">
-      <div class="form-group"><label for="email">Email</label><input id="email" v-model="email" type="text"></div>
-      <div class="form-group"><label for="password">Password</label><input id="password" v-model="password" type="password"></div>
-      <button type="submit">Submit</button>
-    </form>
+  <div class="login-container">
+    <div class="form-container">
+
+      <form action="" class="form">
+        <h1>WELLCOME</h1>
+        <h2>we are glad to see you back with us</h2>
+        <div class="form-group"><label for="email">Email</label><input id="email" v-model="email" type="text"></div>
+        <div class="form-group"><label for="password">Password</label><input id="password" v-model="password" type="password"></div>
+        <button type="submit">LOGIN</button>
+        <p>Don't have an account <span>SIGN UP</span></p>
+      </form>
+    </div>
+    <div class="image-container">
+      <img src="/logo-app.webp" alt="">
+    </div>
   </div>
 </template>
 
@@ -33,4 +42,32 @@ export default {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.login-container{
+  width: clamp(700px, 50vw , 1200px);
+  display: flex;
+  background-color: white;
+  border-radius: 7px;
+  margin-right: auto;
+  margin-left: auto;
+
+}
+.form-container {
+  display: flex;
+  flex-direction: column;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.image-container{
+  flex: 1;
+}
+
+.image-container img {
+  max-width: 100%;
+}
+
+</style>
