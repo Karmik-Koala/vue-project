0432d41b0aeb885c821057c6df6bf2f29df03914
Guillermo Leal <guillermo01.galp@gmail.com>
2023-10-17 10:32:18 +0200

    added pinia in the project

--- src/auth/AuthView.vue
+++ src/auth/AuthView.vue
@@ -1,36 +1,52 @@
 <template>
   <div>
     <form action="">
-      <div class="form-group"><label for="email">Email</label><input id="email" v-model="email" type="text"></div>
-      <div class="form-group"><label for="password">Password</label><input id="password" v-model="password" type="password"></div>
+      <div class="form-group">
+        <label for="email">Email</label
+        ><input id="email" v-model="email" type="text" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label
+        ><input id="password" v-model="password" type="password" />
+      </div>
       <button type="submit">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
-import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useAuthStore } from "./stores/authStore";
+
+const store = useAuthStore();
+
 export default {
-  name: 'AuthView', 
+  name: "AuthView",
   data() {
     return {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: "",
+    };
+  },
+  mounted() {
+    console.log(store.userAuth);
   },
   methods: {
     async authUser() {
       try {
-        const auth = getAuth()
-        const authCredential = await signInWithEmailAndPassword(auth, this.email, this.password)
-        authCredential? alert('logged') : alert('not logged')
+        const auth = getAuth();
+        const authCredential = await signInWithEmailAndPassword(
+          auth,
+          this.email,
+          this.password
+        );
+        authCredential ? alert("logged") : alert("not logged");
       } catch (error) {
-        console.error(error.message)
+        console.error(error.message);
       }
-    }
-  }
-}
-
+    },
+  },
+};
 </script>
 
 <style scoped></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -14,4 +14,12 @@ export const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import("../pages/DetailsMeal.vue"),
   },
+  {
+    path: "/login",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../auth/AuthView.vue"),
+  },
 ];
