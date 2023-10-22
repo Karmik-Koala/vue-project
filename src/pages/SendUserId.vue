068097c59c607ef5d69dad2541df292680587442
Daniel Rodríguez <meazu05@gmail.com>
2023-10-22 21:02:21 +0100

    Añadir a la base de datos la receta con el idusuario

---
+++ src/pages/SendUserId.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+    </div>
+  </template>
+  
+  <script>
+
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { sendUserIdToDatabase } from "@/services/meals.js";
+
+  export default {
+    mounted() {
+      const auth = getAuth();
+  
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const userId = user.uid;
+          sendUserIdToDatabase(userId);
+
+        }
+      });
+    },
+    methods: {
+      
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos opcionales para tu componente */
+  </style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -23,4 +23,12 @@ export const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import("../auth/AuthView.vue"),
   },
+  {
+    path: "/favorites",
+    name: "favorites",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../pages/FavoritesMeals.vue"),
+  },
 ];
