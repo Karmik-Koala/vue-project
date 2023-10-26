5390fcf305c5dcf64d70ab9e5c3eddd83dfe9bfc
Diego M. <morfidal@gmail.com>
2023-10-26 11:10:07 +0200

    Form header styles with tipography

--- src/auth/components/FormHeader.vue
+++ src/auth/components/FormHeader.vue
@@ -25,19 +25,29 @@ export default {
 .form-header {
   position: relative;
   margin-bottom: 20px;
-
-  & .title {
+  .title {
+    font-family: var(--font-family-archivo-black);
     font-size: var(--font-size-xl);
-    margin-bottom: 10px;
+    -webkit-text-stroke-width: 1px;
+    -moz-text-stroke-width: 1px;
+    -webkit-text-stroke-color: var(--color-ferra);
+    -moz-text-stroke-color: var(--color-ferra);
+    text-shadow: 0.3rem 0.3rem var(--color-salmon);
+    color: var(--color-grandis);
     text-align: center;
+    margin-bottom: 5px;
   }
 
   & .subtitle {
-    position: absolute;
-    bottom: -10px;
-    left: 0;
-    right: 0;
-    font-size: var(--font-size-xxs);
+    font-family: var(--font-family-archivo-black);
+    font-size: var(--font-size-xs);
+    background: linear-gradient(
+      to right,
+      var(--color-ferra) 0%,
+      var(--color-ferra-light) 100%
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
     text-align: center;
   }
 }
