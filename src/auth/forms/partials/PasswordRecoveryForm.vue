96530b1905654e33fcbf6cb6a6014687082f0951
Diego M. <morfidal@gmail.com>
2023-10-26 10:23:02 +0200

    password recovery logic works

--- src/auth/forms/partials/PasswordRecoveryForm.vue
+++ src/auth/forms/partials/PasswordRecoveryForm.vue
@@ -3,13 +3,14 @@
     <h2>Recuperación de Correo</h2>
     <p>Ingresa tu correo electrónico y te enviaremos instrucciones para recuperar tu cuenta.</p>
     <CustomInput name="email" type="email"/>
-    <button @click="fnAlert">Recuperar Contraseña</button>
+    <button @click="resetPassword">Recuperar Contraseña</button>
+    <button @click="goToLogin">LOGIN</button>
   </div>
 </template>
 
 <script setup>
-import { projectAuth } from "@/firebase/config";
 import { useForm } from "vee-validate";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 import CustomInput from "../../components/CustomInput.vue";
 import { passwordRecoveryValidationSchema } from '../schemas/passwordRecovery.scheme'
 
@@ -17,16 +18,11 @@ const {values} = useForm({
   validationSchema: passwordRecoveryValidationSchema
 })
 
-const fnAlert = () => alert('hola')
 const resetPassword = () => {
-  projectAuth
-    .sendPasswordResetEmail(values.email)
-    .then(() => {
-      alert("Se han enviado instrucciones a tu correo electrónico.");
-    })
-    .catch((error) => {
-      console.error(error.message);
-      alert("Ha ocurrido un error. Verifica tu dirección de correo electrónico.");
-    });
+  
+  sendPasswordResetEmail(getAuth(), values.email)
+    .then(() => alert('mail enviado'), () => alert('error envio'))
 };
+
+const goToLogin = () => { alert('hola ka se')}
 </script>
